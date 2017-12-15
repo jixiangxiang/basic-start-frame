a6ea67b085de2bd5b5f16b58c5af3b23e0663c0e
jixiangxiang <jixiangxiang@haier.com>
2017-12-15 13:52:16 +0800

    去除Gson转换器

--- baselibrary/src/main/java/com/eric/self/baselibrary/http/retrofit/RetrofitManager.java
+++ baselibrary/src/main/java/com/eric/self/baselibrary/http/retrofit/RetrofitManager.java
@@ -78,7 +78,6 @@ public class RetrofitManager {
                 .baseUrl((String) SPUtils.get(context, BASE_URL, ""))
                 .client(mOkHttpClient)
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
                 .addConverterFactory(FastJsonConverterFactory.create())
                 .build();
     }
